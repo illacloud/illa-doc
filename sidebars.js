0436142608a7d4532fb11d1dacbbd3c9b120760d
Mikiko <112603073+wenzhuo2022@users.noreply.github.com>
2023-10-27 17:30:07 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -9,102 +9,9 @@ const sidebars = {
       label: 'build_apps',
       link: {
         type: 'doc',
-        id: "form"
+        id: "run-script"
       },
       items: [
-        {
-          type: 'category',
-          label: 'components',
-          items: [
-            {
-              type: 'category',
-              label: 'text_inputs',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'Number_inputs',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'select_inputs',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'date_time_input',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'special_input',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'container_and_form',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'presentation',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'data_presentation',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'navigation',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'media_presentation',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'special_presentation',
-              items: ["text"]
-            },
-          ]
-        },
-        {
-          type: 'category',
-          label: 'resources',
-          items: [
-            {
-              type: 'category',
-              label: 'overview',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'allow_access_from_ILLA',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'database',
-              items: ["text"]
-            },
-            {
-              type: 'category',
-              label: 'apis',
-              items: ["text"]
-            },
-          ]
-        },
-        "text-area-input",
-        {
-          type: 'category',
-          label: 'global',
-          items: ["text"]
-        },
         {
           type: 'category',
           label: 'JavaScript_and_Event_handler',
@@ -115,26 +22,8 @@ const sidebars = {
     {
       type: 'category',
       label: 'ILLA_with_ai',
-      items: ["text"]
+      items: ["ai-agent"]
     },
-    {
-      type: 'category',
-      label: 'teams_and_users',
-      items: ["text"]
-    },
-    {
-      type: 'category',
-      label: 'changelog',
-      items: ["text"]
-    },
-    {
-      type: 'category',
-      label: 'test',
-      items: ["text-area-input"]
-    },
-    "tidb",
-    "time-picker",
-    "time-range",
   ],
 };
 
